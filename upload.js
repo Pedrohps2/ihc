b9d499f1ab4241e224c3c962885de2ef7f232db4
Pedrohps2 <135916313+Pedrohps2@users.noreply.github.com>
2023-06-07 19:45:15 -0300

    Add files via upload

---
+++ upload.js
@@ -0,0 +1,46 @@
+let fileInput = document.getElementById("upload");
+let fileList = document.getElementById("files-list");
+let numOfFiles = document.getElementById("num-of-files");
+
+fileInput.addEventListener("change",()=>{
+    fileList.innerHTML = "";
+    numOfFiles.textContent = `${fileInput.files.length} Selecionado(s)`;
+})
+
+window.addEventListener("load",()=>{
+    const input = document.getElementById("upload");
+    const filewrapper = document.getElementById("filewrapper");
+
+    input.addEventListener("change",(e)=>{
+        let fileName = e.target.files[0].name;
+        let filetype = e.target.value.split(".").pop();
+        fileshow(fileName, fileName);
+    })
+
+    const fileshow = (fileName, filetype) =>{
+        const showfileboxElem = document.createElement("div");
+        showfileboxElem.classList.add("showfilebox");
+        const leftElem = document.createElement("div");
+        leftElem.classList.add("left");
+        const fileTypeElem = document.createElement("span");
+        fileTypeElem.classList.add("filetype");
+        fileTypeElem.innerHTML=filetype;
+        leftElem.append(fileTypeElem);
+        const filetitleElem = document.createElement("h3");
+        filetitleElem.innerHTML=fileName;
+        leftElem.append(filetitleElem);
+        showfileboxElem.append(leftElem);
+        const rightElem = document.createElement("div");
+        rightElem.classList.add("right");
+        showfileboxElem.append(rightElem);
+        const crossElem = document.createElement("span");
+        crossElem.innerHTML = "&#215;";
+        rightElem.append(crossElem);
+        filewrapper.append(showfileboxElem);
+
+        crossElem.addEventListener("click", ()=>{
+            filewrapper.removeChild(showfileboxElem);
+        })
+    }
+
+})
